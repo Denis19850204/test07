4a7c60380cccdbfed2fbb22c84e433b3fa937ce5
Denis19850204 <dg04021985@gmail.com>
2022-10-13 08:02:58 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  
+  console.log(e.target.dataset.source)
+}
+
+const galleryCard = createGalleryCard();
+
+function createGalleryCard() {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", galleryCard);
+
+// const instance = basicLightbox.create();
+// instance.show()
