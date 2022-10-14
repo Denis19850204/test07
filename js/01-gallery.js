5845411a77e9ac7ca0d813b61675c401b7c9d596
Denis19850204 <dg04021985@gmail.com>
2022-10-14 13:49:03 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,15 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
+
 gallery.addEventListener("click", onClick);
 
-function onClick(e) {
-  e.preventDefault();
-  if (!e.target.classList.contains("gallery__image")) {
-    return;
-  }
-  
-  console.log(e.target.dataset.source)
-}
-
-const galleryCard = createGalleryCard();
-
-function createGalleryCard() {
-  return galleryItems
-    .map(({ description, original, preview }) => {
-      return `<div class="gallery__item">
+const galleryCard = galleryItems
+  .map(({ description, original, preview }) => {
+    return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -30,11 +19,29 @@ function createGalleryCard() {
     />
   </a>
 </div>`;
-    })
-    .join("");
-}
+  })
+  .join("");
 
 gallery.insertAdjacentHTML("beforeend", galleryCard);
 
-// const instance = basicLightbox.create();
-// instance.show()
+function onClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const imgSrc = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<img src=${imgSrc} width="800" height="600">`
+  );
+  instance.show();
+
+  window.addEventListener("keydown", onEscape);
+
+  function onEscape(e) {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+
+
+const galleryCard = galleryItems
+  .map(({ description, original, preview }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryCard);
+
+let lightbox = new SimpleLightbox('.gallery a', {});
+lightbox.options.captionsData = 'alt';
+lightbox.options.captionDelay = '250';
+
+
+
